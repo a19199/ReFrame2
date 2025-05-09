359b42702f166972148fbc5435d0fc5b5dcc1399
a19199 <a19199@gmail.com>
2025-05-08 21:46:43 -0500

    Update submit-order.js

--- pages/api/submit-order.js
+++ pages/api/submit-order.js
@@ -1,21 +1,25 @@
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
+    return res.status(405).end('Method Not Allowed');
   }
 
+  const { name, email, frame, image } = req.body;
+
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbypNstRxp0jUUagIjpTeIuX3NJ56_tox2qYly2PPRIUqvFM2NJ-SIfei595pFtar1xw/exec', {
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbwmYs4IVe6mHF-Jw7SmXQfrSYoWkwLKfs5oYooUqLKkGQf7Bopdj9qFPTNlVInbK8Qa/exec';
+
+    const response = await fetch(scriptUrl, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(req.body),
+      body: JSON.stringify({ name, email, frame, image }),
     });
 
     if (!response.ok) {
-      throw new Error('Failed to submit order to Google Sheets');
+      throw new Error('Failed to send to Google Sheets');
     }
 
     res.status(200).json({ message: 'Order submitted successfully' });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ error: error.message });
   }
 }
